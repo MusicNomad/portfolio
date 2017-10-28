125b47ed3a2d6aa8b3ed27ecd0536030634ec24c
MusicNomad <musicnomad@users.noreply.github.com>
2017-10-27 18:08:10 -0700

    First commit of CastSpells
    Signed-off-by: MusicNomad <ateeb90@gmail.com>

---
+++ Larry DeMaio Lab 2/src/CastSpells.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CastSpells {
+
+    private static Scanner keyboard = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
